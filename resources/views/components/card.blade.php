a8e3032e265d6540be9ea236ef6b145ce6dfad1c
Peter Cornelis <peter.cornelis86@gmail.com>
2026-03-24 18:56:37 +0100

    added fonts locally

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,6 +4,6 @@
     <div class="relative text-center p-4">
         <h3 class="text-xl font-semibold">{{ $movie->title }}</h3>
         <p class="text-sm text-foreground/50">Released: {{ $movie->release_year }}</p>
-        <div class="absolute flex items-center justify-center text-sm font-bold -top-8 right-1/2 translate-x-1/2 bg-card p-2 aspect-square rounded-full shadow-md shadow-black/50 border-2 ring ring-card {{ $movie->vote_average >= 7 ? 'border-green-600' : ($movie->vote_average >= 4 ? 'border-yellow-600' : 'border-red-600') }}">{{ $movie->rating }}</div>
+        <div class="absolute flex items-center justify-center text-sm font-bold -top-8 right-1/2 translate-x-1/2 bg-card p-2 aspect-square rounded-full shadow-md shadow-black/50 border-2 ring-2 ring-card {{ $movie->vote_average >= 7 ? 'text-emerald-400 border-emerald-400' : ($movie->vote_average >= 4 ? 'text-yellow-400 border-yellow-400' : 'text-red-400 border-red-400') }}">{{ $movie->rating }}</div>
     </div>
 </div>
