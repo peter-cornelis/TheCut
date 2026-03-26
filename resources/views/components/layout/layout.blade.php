2d42318d2025b9019149bd207c0c078b41650263
Peter Cornelis <peter.cornelis86@gmail.com>
2026-03-26 13:55:17 +0100

    fix oprhaned div

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -16,11 +16,10 @@
             {{ $slot }}
         </main>
         <footer class="px-4 border-t border-border text-center py-4 text-sm text-muted-foreground font-medium">
-                &copy; {{ date('Y') }} | Peter Cornelis | {{ __('pages.meta_provided') }}
-                <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
-                    <img src="/images/tmdb_logo.svg" alt="TMDB Logo" class="ml-1 inline h-4">
-                </a>
-            </div>
+            &copy; {{ date('Y') }} | Peter Cornelis | {{ __('pages.meta_provided') }}
+            <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
+                <img src="/images/tmdb_logo.svg" alt="TMDB Logo" class="ml-1 inline h-4">
+            </a>
         </footer>
     </body>
 </html>
